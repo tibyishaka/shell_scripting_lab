e0dc54bafc80bc9aa0d8181604395135200fd3ec
ibyishaka <t.ibyishaka@alustudent.com>
2024-10-03 12:05:07 +0200

    adding

---
+++ create_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+mkdir -p submission_reminder_app
+
+mkdir -p submission_reminder_app/app
+mkdir -p submission_reminder_app/modules
+mkdir -p submission_reminder_app/assets
+mkdir -p submission_reminder_app/config
+touch submission_reminder_app/startup.sh
+
+touch submission_reminder_app/app/reminder.sh 
+touch submission_reminder_app/modules/functions.sh
+touch submission_reminder_app/assets/submissions.txt
+touch submission_reminder_app/config/config.env
+
